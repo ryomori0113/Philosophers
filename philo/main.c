5db89225f92938cc70a40b054ffeed970c76da3a
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2024-12-01 15:38:02 +0900

    philo

--- philo/main.c
+++ philo/main.c
@@ -16,19 +16,36 @@ unsigned int	get_ms_time(void)
 
 void func_think(t_philo philo)
 {
-    printf("%d  %d is thinking\n", get_ms_time(), philo.id);
+    printf("%u  %u is thinking\n", get_ms_time(), philo.id);
+
+    if(philo.id % 2 == 0)
+    {
+        pthread_mutex_lock(philo.right_fork);
+        pthread_mutex_lock(philo.left_fork);
+    }
+    else
+    {
+        pthread_mutex_lock(philo.left_fork);
+        pthread_mutex_lock(philo.right_fork);
+    }
+    // printf("%d  %d is thinking finished!!!  \n", get_ms_time(), philo.id);
 }
 
 void func_eat(t_philo philo)
 {
-    printf("%d  %d is eating\n", get_ms_time(), philo.id);
-    usleep(3000000);
+    printf("%u  %u is eating\n", get_ms_time(), philo.id);
+    usleep(300000);//定数
+
+    pthread_mutex_unlock(philo.left_fork);
+    pthread_mutex_unlock(philo.right_fork);
+    // printf("%d  %d is eating finished!!! \n", get_ms_time(), philo.id);
 }
 
 void func_sleep(t_philo philo)
 {
-    printf("%d  %d is sleeping\n", get_ms_time(), philo.id);
-    usleep(3000000);
+    printf("%u  %u is sleeping\n", get_ms_time(), philo.id);
+    usleep(300000);//定数
+    // printf("%d  %d is sleeping finished!!! \n", get_ms_time(), philo.id);
 }
 
 static void * dine(void *arg)
@@ -37,7 +54,7 @@ static void * dine(void *arg)
     t_philo *philo = (t_philo *)arg;
 	// fprintf(stderr, "philo [%d], left: %p, right: %p\n", philo->id, philo->left_fork, philo->right_fork);
     i = 0;
-    while(i < philo->count)
+    while(i < philo->count)//?5times??例死ぬまでループの可能性もある(何回食べるかの回数ぶん回すoption有)
     {
         func_think(*philo);
         func_eat(*philo);
@@ -56,18 +73,19 @@ int    main(void)
    t_philo     philo[5];
 //    pthread_mutex_t fork[5];
    int philo_num = 5;
-	static pthread_mutex_t fork[5] = {PTHREAD_MUTEX_INITIALIZER};
+	static pthread_mutex_t fork[5] = {PTHREAD_MUTEX_INITIALIZER};//init + dedach
 
 
    i = 0;
    while(i < philo_num)
    {
-        printf("i: %d\n", i);
+        // printf("i: %d\n", i);
        philo[i].id = i + 1;
-       philo[i].left_fork = fork[i];
-       philo[i].right_fork = fork[(i+1)%philo_num];
+       philo[i].left_fork = &fork[i];
+       philo[i].right_fork = &fork[(i+1)%philo_num];
        philo[i].count = philo_num;
        pthread_create(&thread_id[i], NULL, dine, &(philo[i]));
+       usleep(20000);
        i++;
    }
    sleep(10);
--- philo/philo.h
+++ philo/philo.h
@@ -6,8 +6,8 @@
 typedef struct s_philo
 {
     int     id;
-    pthread_mutex_t left_fork;
-    pthread_mutex_t right_fork;
+    pthread_mutex_t *left_fork;
+    pthread_mutex_t *right_fork;
     int     count;
     unsigned int  eat_time;
     unsigned int    sleep_time;    
