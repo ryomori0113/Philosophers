83f8c43a349a9e3c54f00790fca8ac7fdb363ac9
Ryosuke Mori <ryomori@c2r7s1.42tokyo.jp>
2024-12-03 17:32:02 +0900

    :+1:引数が4つのとき死ぬまでor永遠ループ

--- philo/main.c
+++ philo/main.c
@@ -74,28 +74,31 @@ void func_sleep(t_philo *philo)
 
 void *monitor_philosophers(void *arg)
 {
-    t_philo *philosophers = (t_philo *)arg;
-    t_shared *shared = philosophers[0].shared;
+	int i;
+    t_philo *philo = (t_philo *)arg;
+    t_shared *shared = philo[0].shared;
 
     while (!is_simulation_stopped(shared))
     {
-        for (int i = 0; i < shared->philo_num; i++)
+		i = 0;
+        while (i < shared->philo_num)
         {
-            pthread_mutex_lock(&philosophers[i].meal_mutex);
+            pthread_mutex_lock(&philo[i].meal_mutex);
             unsigned int current_time = get_ms_time();
 
-            if (current_time - philosophers[i].last_meal_time > shared->time_to_die)
+            if (current_time - philo[i].last_meal_time > shared->time_to_die)
             {
                 pthread_mutex_lock(&shared->stop_mutex);
                 shared->simulation_stop = true;
                 pthread_mutex_unlock(&shared->stop_mutex);
 
-                printf("%u %d died\n", current_time, philosophers[i].id);
-                pthread_mutex_unlock(&philosophers[i].meal_mutex);
+                printf("%u %d died\n", current_time, philo[i].id);
+                pthread_mutex_unlock(&philo[i].meal_mutex);
                 return NULL;
             }
 
-            pthread_mutex_unlock(&philosophers[i].meal_mutex);
+            pthread_mutex_unlock(&philo[i].meal_mutex);
+			i++;
         }
         usleep(1000);
     }
@@ -110,12 +113,13 @@ void *dine(void *arg)
         usleep(500);
     }
 
-    while (!is_simulation_stopped(philo->shared) && philo->count >= 0)
+    while (!is_simulation_stopped(philo->shared) && (philo->count == -1 || philo->count > 0))
     {
         func_think(philo);
         func_eat(philo);
         func_sleep(philo);
-        philo->count--;
+        if (philo->count > 0)
+    		philo->count--;
     }
     return NULL;
 }
@@ -135,7 +139,9 @@ int parse_argument(const char *arg)
 // メイン関数
 int main(int argc, char **argv)
 {
-    if (argc != 6)
+	int i;
+
+    if (argc != 5 && argc != 6)
     {
         fprintf(stderr, "Usage: %s [philo_num] [time_to_die] [time_to_eat] [time_to_sleep] [meal_count]\n", argv[0]);
         return 1;
@@ -145,14 +151,19 @@ int main(int argc, char **argv)
     unsigned int time_to_die = parse_argument(argv[2]);
     unsigned int time_to_eat = parse_argument(argv[3]);
     unsigned int time_to_sleep = parse_argument(argv[4]);
-    int meal_count = parse_argument(argv[5]);
+    int meal_count;
+
+	if(argc == 6)
+		meal_count = parse_argument(argv[5]);
+	else
+		meal_count = -1;
 
     pthread_t *threads = malloc(sizeof(pthread_t) * philo_num);
     pthread_mutex_t *forks = malloc(sizeof(pthread_mutex_t) * philo_num);
-    t_philo *philos = malloc(sizeof(t_philo) * philo_num);
+    t_philo *philo = malloc(sizeof(t_philo) * philo_num);
     t_shared shared_data;
 
-    if (!threads || !forks || !philos)
+    if (!threads || !forks || !philo)
     {
         fprintf(stderr, "Error: Memory allocation failed.\n");
         return 1;
@@ -165,44 +176,52 @@ int main(int argc, char **argv)
     shared_data.simulation_stop = false;
     pthread_mutex_init(&shared_data.stop_mutex, NULL);
 
-    for (int i = 0; i < philo_num; i++)
+	i = 0;
+    while (i < philo_num)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+	
+	i = 0;
+    while (i < philo_num)
     {
-        pthread_mutex_init(&forks[i], NULL);
-    }
-
-    for (int i = 0; i < philo_num; i++)
-    {
-        philos[i].id = i + 1;
-        philos[i].left_fork = &forks[i];
-        philos[i].right_fork = &forks[(i + 1) % philo_num];
-        philos[i].count = meal_count;
-        philos[i].shared = &shared_data;
-        philos[i].last_meal_time = get_ms_time();
-        pthread_mutex_init(&philos[i].meal_mutex, NULL);
-        pthread_create(&threads[i], NULL, dine, &philos[i]);
+        philo[i].id = i + 1;
+        philo[i].left_fork = &forks[i];
+        philo[i].right_fork = &forks[(i + 1) % philo_num];
+        philo[i].count = meal_count;
+        philo[i].shared = &shared_data;
+        philo[i].last_meal_time = get_ms_time();
+        pthread_mutex_init(&philo[i].meal_mutex, NULL);
+        pthread_create(&threads[i], NULL, dine, &philo[i]);
         usleep(20000);
+		i++;
     }
 
     pthread_t monitor_thread;
-    pthread_create(&monitor_thread, NULL, monitor_philosophers, philos);
+    pthread_create(&monitor_thread, NULL, monitor_philosophers, philo);
 
-    for (int i = 0; i < philo_num; i++)
+	i = 0;
+    while(i < philo_num)
     {
         pthread_join(threads[i], NULL);
+		i++;
     }
 
     pthread_join(monitor_thread, NULL);
 
-    for (int i = 0; i < philo_num; i++)
+	i = 0;
+    while(i < philo_num)
     {
         pthread_mutex_destroy(&forks[i]);
-        pthread_mutex_destroy(&philos[i].meal_mutex);
+        pthread_mutex_destroy(&philo[i].meal_mutex);
+		i++;
     }
     pthread_mutex_destroy(&shared_data.stop_mutex);
 
     free(threads);
     free(forks);
-    free(philos);
+    free(philo);
 
     return 0;
 }
